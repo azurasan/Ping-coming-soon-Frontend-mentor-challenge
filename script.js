dc4b5ca50920f0f26c3ccae6bae082d33bf412d2
azurasan <ivanjenius82@gmail.com>
2021-02-04 16:35:08 +0700

    delete media query for event listener

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const warning = document.querySelector('.warning');
 const btn = document.querySelector('button');
 const mediaQuery = window.matchMedia('min-width:1200px');
 
-if(mediaQuery){
-    console.log('Media Query Matched !');
-    btn.addEventListener('mouseover', (e) => {
+
+
+btn.addEventListener('mouseover', (e) => {
         // prevent send data
         e.preventDefault();
 
@@ -24,46 +24,3 @@ if(mediaQuery){
             }
         }
     });
-} else{
-    console.log("Media Query doesn't match !");
-
-    btn.addEventListener('click', (e) => {
-        // prevent send data
-        e.preventDefault();
-
-        // if input is empty,
-        if (inputEmail.value === '') {
-            warning.classList.add('active');
-            inputEmail.classList.add('active');
-            warning.innerHTML = 'Email cannot be empty !';
-        } else {
-            warning.classList.remove('active');
-            inputEmail.classList.remove('active');
-            if (inputEmail.validity.valid === false) {
-                warning.classList.add('active');
-                inputEmail.classList.add('active');
-                warning.innerHTML = 'Please provide a valid email address';
-            }
-        }
-    });
-}
-
-// btn.addEventListener('mouseover', (e) => {
-//         // prevent send data
-//         e.preventDefault();
-
-//         // if input is empty,
-//         if (inputEmail.value === '') {
-//             warning.classList.add('active');
-//             inputEmail.classList.add('active');
-//             warning.innerHTML = 'Email cannot be empty !';
-//         } else {
-//             warning.classList.remove('active');
-//             inputEmail.classList.remove('active');
-//             if (inputEmail.validity.valid === false) {
-//                 warning.classList.add('active');
-//                 inputEmail.classList.add('active');
-//                 warning.innerHTML = 'Please provide a valid email address';
-//             }
-//         }
-//     });
